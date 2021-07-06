7aa7dc41715a28937e1450def7deeebf213822ec
Emek Taydaş <emektaydas@hotmail.com>
2021-07-06 18:09:27 +0300

    CanTurn Test

--- Test/MarsTests.cs
+++ Test/MarsTests.cs
@@ -6,21 +6,45 @@ namespace Test
     public class MarsTests
     {
         private Rover rover;
+        private Coordinate initialCoordinates;
 
         [SetUp]
         public void Setup()
         {
             rover = new Rover();
+            initialCoordinates = new Coordinate(0, 0, Direction.North);
         }
 
         [Test]
         public void SetInitialCoordinate()
         {
-            var initialCoordinates = new Coordinate(0, 0, Direction.North);
-
             rover.Coordinates = initialCoordinates;
-
             Assert.AreEqual(rover.Coordinates, initialCoordinates);
         }
+
+        [Test]
+        public void CanTurnLeft()
+        {
+            // reset to initial
+            rover.Coordinates = initialCoordinates;
+            rover.Turn(App.Way.Left);
+
+            Assert.IsTrue(rover.Coordinates.X == 0);
+            Assert.IsTrue(rover.Coordinates.Y == 0);
+            Assert.IsTrue(rover.Coordinates.Direction == Direction.West);
+        }
+
+        [Test]
+        public void CanTurnRight()
+        {
+            // reset to initial
+            rover.Coordinates = initialCoordinates;
+            rover.Turn(App.Way.Right);
+
+            Assert.IsTrue(rover.Coordinates.X == 0);
+            Assert.IsTrue(rover.Coordinates.Y == 0);
+            Assert.IsTrue(rover.Coordinates.Direction == Direction.East);
+        }
     }
+}
 }
